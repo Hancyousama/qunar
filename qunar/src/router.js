238e59ea3ed2074b87588aa1c3996d6927f559f0
Hancyou <hancyousama@qq.com>
2019-02-15 01:41:13 +0800

    Initialization

--- qunar/src/router.js
+++ qunar/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import List from '@/pages/list/list.vue'
 import Home from '@/pages/home/home.vue'
 Vue.use(Router)
 
@@ -13,20 +12,6 @@ export default new Router({
       name: 'home',
       component: Home
       // component: () => import(/* webpackChunkName: "about" */ '@/pages/home/home.vue')
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: List
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-
   ]
 })
